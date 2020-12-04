f5010480be59b4b040b6c490e43565aec40e06ec
RishabhGarg108 <rishabhgarg108@gmail.com>
2020-12-04 13:35:45 +0530

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# designauts_webpage
---
+++ precache-manifest.86119d86bf70782a100c4584802b1e61.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6529322b4d9aeb2ce6eb75038ee20739",
+    "url": "/designauts_webpage/index.html"
+  },
+  {
+    "revision": "29c6bb4d8ae7e278cd5e",
+    "url": "/designauts_webpage/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f7d184ada615830228ba",
+    "url": "/designauts_webpage/static/css/main.fcd12773.chunk.css"
+  },
+  {
+    "revision": "29c6bb4d8ae7e278cd5e",
+    "url": "/designauts_webpage/static/js/2.e3d4458d.chunk.js"
+  },
+  {
+    "revision": "433290581eb2286c565cededd6d282b7",
+    "url": "/designauts_webpage/static/js/2.e3d4458d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7d184ada615830228ba",
+    "url": "/designauts_webpage/static/js/main.fef30230.chunk.js"
+  },
+  {
+    "revision": "5bc5f9536ad811a36d32",
+    "url": "/designauts_webpage/static/js/runtime-main.2e1043b9.js"
+  },
+  {
+    "revision": "1a9eb0bb9d33aa22af8b1d44980e57da",
+    "url": "/designauts_webpage/static/media/background.1a9eb0bb.jpeg"
+  },
+  {
+    "revision": "8020692b6d0d3d4aa59676f1d70acd83",
+    "url": "/designauts_webpage/static/media/logo.8020692b.png"
+  }
+]);
